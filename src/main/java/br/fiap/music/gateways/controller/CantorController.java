13417454e5b353f1362e02ca175837071c2d8a49
Henrique Batista <hbshenrique.ti@gmail.com>
2026-04-06 04:27:53 -0300

    Controller refeitos

---
+++ src/main/java/br/fiap/music/gateways/controller/CantorController.java
@@ -0,0 +1,73 @@
+package br.fiap.music.gateways.controller;
+
+import br.fiap.music.domain.Cantor;
+import br.fiap.music.domain.Cliente;
+import br.fiap.music.gateways.dto.CantorDTO;
+import br.fiap.music.gateways.dto.ClienteDTO;
+import br.fiap.music.service.CantorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/api/cantores")
+@RequiredArgsConstructor
+public class CantorController {
+    private final CantorService service;
+
+    @PostMapping
+    public ResponseEntity<EntityModel<CantorDTO>> create(@RequestBody @Valid CantorDTO body) {
+
+        CantorDTO cantor = service.create(body);
+
+        EntityModel<CantorDTO> entityModel = EntityModel.of(cantor,
+                linkTo(methodOn(CantorController.class).get(cantor.id())).withSelfRel());
+
+        return ResponseEntity.created(entityModel.getRequiredLink("self").toUri())
+                .body(entityModel);
+    }
+
+    @GetMapping
+    public ResponseEntity<EntityModel<List<CantorDTO>>> list() {
+        EntityModel<List<CantorDTO>> response = EntityModel.of(service.list());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<Cantor>> get(@PathVariable Long id) {
+        Cantor cantor = service.get(id);
+
+        EntityModel<Cantor> cantorModel = EntityModel.of(cantor,
+                linkTo(methodOn(CantorController.class).get(id)).withSelfRel(),
+                linkTo(methodOn(CantorController.class).list()).withRel("lista-cantores"));
+
+        return ResponseEntity.ok(cantorModel);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<Cantor>> update(@PathVariable Long id, @RequestBody @Valid CantorDTO body) {
+        Cantor cantor = service.update(id, body);
+
+        EntityModel<Cantor> cantorModel = EntityModel.of(cantor,
+                linkTo(methodOn(CantorController.class).update(id, null)).withSelfRel(),
+                linkTo(methodOn(CantorController.class).list()).withRel("lista-cantores")
+        );
+
+        return ResponseEntity.ok(cantorModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/br/fiap/music/gateways/controller/MusicaController.java
@@ -0,0 +1,76 @@
+package br.fiap.music.gateways.controller;
+
+import br.fiap.music.domain.Cliente;
+import br.fiap.music.domain.Musica;
+import br.fiap.music.gateways.dto.ClienteDTO;
+import br.fiap.music.gateways.dto.MusicaDTO;
+import br.fiap.music.service.MusicaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/api/musicas")
+@RequiredArgsConstructor
+public class MusicaController {
+    private final MusicaService service;
+
+    @PostMapping
+    public ResponseEntity<EntityModel<MusicaDTO>> create(@RequestBody @Valid MusicaDTO body) {
+
+        MusicaDTO musica = service.create(body);
+
+        EntityModel<MusicaDTO> entityModel = EntityModel.of(musica,
+                linkTo(methodOn(MusicaController.class).get(musica.id())).withSelfRel());
+
+        return ResponseEntity.created(entityModel.getRequiredLink("self").toUri())
+                .body(entityModel);
+    }
+
+    @GetMapping
+    public ResponseEntity<EntityModel<List<MusicaDTO>>> list() {
+        EntityModel<List<MusicaDTO>> response = EntityModel.of(service.list());
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<MusicaDTO>> get(@PathVariable Long id) {
+        EntityModel<MusicaDTO> response = EntityModel.of(service.get(id),
+                linkTo(methodOn(MusicaController.class).get(id)).withSelfRel(),
+                linkTo(methodOn(MusicaController.class).list()).withRel("lista-musicas"));
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<MusicaDTO>> update(@PathVariable Long id, @RequestBody @Valid MusicaDTO body) {
+        MusicaDTO musica = service.update(id, body);
+
+        EntityModel<MusicaDTO> cantorModel = EntityModel.of(musica,
+                linkTo(methodOn(MusicaController.class).update(id, null)).withSelfRel(),
+                linkTo(methodOn(MusicaController.class).list()).withRel("lista-musicas")
+        );
+
+        return ResponseEntity.ok(cantorModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/search")
+    public List<MusicaDTO> search(@RequestParam String q) {
+        return service.searchTitulo(q);
+    }
+}
